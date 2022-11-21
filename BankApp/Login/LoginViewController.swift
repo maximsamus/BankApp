668928079e0286a8429003521a9458afea420d35
maximsamus <maximsamus1@gmail.com>
2022-11-21 18:51:02 +0300

    update view

---
+++ BankApp/Login/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Максим Самусь on 21.11.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+        ])
+    }
+}
